17df5905fd5f4990f74553dc1d58445ea327513f
Tomás Cruz <82375606+tomascoelhocruz@users.noreply.github.com>
2021-07-20 13:40:41 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+window.addEventListener('keydown', playSound);
